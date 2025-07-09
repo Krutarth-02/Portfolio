e446301071347a326e555cd4fe98944aecf26b49
Krutarth-02 <krtuarthkhadodiya2@gmail.com>
2025-07-09 14:21:16 +0530

    style.css and Javascript updated

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-gsap.from(".navbar", {
-    duration: 1,    
+t  = gsap.timeline();
+t.from(".nm", {
+    duration: 2.5,    
     y: -100,
-    opacity: 0,
-    ease: "power2.out"
+    opacity: 5,
+    rotate:360,
+    // ease: "power2.out"
+});
+t.from("li",{
+    duration:0.5,
+    scale:5,
+    x:700,
+    stagger:0.5,
+})
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
 });
